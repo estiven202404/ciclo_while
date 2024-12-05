d6d1d3bc6d8b3674145e6085fe6e78356db702f2
estiven202404 <yeisonc8@gmail.com>
2024-12-04 19:23:12 -0500

    primeros ejercicios
    primeros ejercicios de la aplicacion

--- cliclo.py
+++ cliclo.py
@@ -3,4 +3,30 @@
 #while condiccion:
         #bloque de codigo a repetir
 
+#definir una variable que lleve el conrol del ciclo
+# numero = 1
+# while numero <= 5: #condicion para iterar n veces...
+#     print (numero)
+#     numero += 1
 
+
+
+
+#contaremos hacia atras
+# numero = 10
+
+# while numero >= 1:
+#     print(numero)
+#     numero -= 1
+#crear un programa que sume los numeros ingresados por el usuario hasta que el usuario ingrese 0
+
+# suma = 0
+
+# numero = int(input("ingresa un numero o pulsa 0 para salir: "))
+
+
+# while numero != 0:
+#     suma += numero
+#     numero = int(input("ingresa un numero o pulsa 0 para salir: "))
+
+#     print(f"la suma total es: {suma}")
